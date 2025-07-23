7cd7c8e3beddbfbf6ee34676f4f2d8dd26dc5f84
Gulshan <gulshan.kumar.dsa@gmail.com>
2025-07-24 02:02:31 +0300

    Added home, contact component and material ui

---
+++ src/app/shared/material.ts
@@ -0,0 +1,7 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+export const material = [
+  MatToolbarModule,
+  MatButtonModule
+];

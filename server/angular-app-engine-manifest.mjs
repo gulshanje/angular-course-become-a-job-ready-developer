5711df40bb7f10805e7cac8c9a0b72c09d902e4d
gulshanje <87398024+gulshanje@users.noreply.github.com>
2025-07-23 23:03:37 +0000

    Deploying to main-gh-pages from @ gulshanje/angular-course-become-a-job-ready-developer@7cd7c8e3beddbfbf6ee34676f4f2d8dd26dc5f84 🚀

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: '/angular-course-become-a-job-ready-developer',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
